de6b05b8b65283e3bd05990ab6e1f875b5191f28
MaksZhylyk <maxa.zhilik@gmail.com>
2024-11-28 13:55:02 +0100

    homework06 change file type jsx - tsx

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,41 @@
+import "./style.css";
+import Button from "../Button/Button";
+
+//Step 1 - import hook useState from react
+import { useState } from "react";
+
+function Counter() {
+  console.log("Render");
+
+  //Step 2 - call hook useState and give him initialState
+  //
+  const [count, setCount] = useState(0);
+  //const result - useState(0);
+  //console.log(result);
+  //const count = result[0];
+  //const setCount = result[1];
+  //console.log(setCount);
+
+  //Step 3 - have to write function which will change condition. In there functions we have to use setCont
+  const onPlusClick = (): void => {
+    setCount((prevValue) => prevValue + 1);
+  };
+
+  const onMinusClick = (): void => {
+    setCount((prevValue) => prevValue - 1);
+  };
+
+  return (
+    <div className="counter-container">
+      <div className="button-wrapper">
+        <Button name="-" onClick={onMinusClick} />
+      </div>
+      <div className="counter-result">{count}</div>
+      <div className="button-wrapper">
+        <Button name="+" onClick={onPlusClick} />
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
